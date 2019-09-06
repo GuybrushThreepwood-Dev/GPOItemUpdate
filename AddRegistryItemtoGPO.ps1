8fdec881dd868fd516b62185b624c65b09f31188
GuybrushThreepwood-Dev <54991104+GuybrushThreepwood-Dev@users.noreply.github.com>
2019-09-06 15:37:54 +0100

    Add files via upload

---
+++ AddRegistryItemtoGPO.ps1
@@ -0,0 +1,9 @@
+﻿### This script will create a registry item for the GPO specificed in the $GPOName variable
+### -Context can be changed depending on whether this is "user" or "computer" configuring GPO
+### -Action sets whether the key will be an Update, Create, Replace, or Delete item
+### -Type sets whether the item is a string, DWord, ExpandString, Binary, MultiString, ExpandString, or QWord
+### If creating multiple entries in the same GPO, then additional lines can be created
+
+$GPOName = "GPOName"
+
+Set-GPPrefRegistryValue -Name $GPOName -Context User -Action Update -Key "HKCU\software\policies\microsoft\office\16.0\clip organizer" -Valuename "RegItemName" -Value 1 -Type DWord
